0726d2f98db4abada48d57eb806749a65f3b4c90
Reyk Mrm <8428349+reykmrm@users.noreply.github.com>
2022-07-31 12:17:16 -0500

    se agrego el baner del video y la configuracion de este para cecular y pc

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vuejs-accessibility/media-has-caption -->
 <template>
   <div class="home">
     <div class="contendor-baner">
@@ -18,6 +19,21 @@
       <ComponetCardSeccionesVue />
       <ComponetCardSeccionesVue />
     </div>
+    <div class="contenedor-baner-portafolio">
+      <div class="contenedor-baner-portafolio-texto">
+        <h1>Titulo</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, consequatur quae.
+          Perferendis commodi expedita dolor itaque rerum corporis eum eveniet, hic, laboriosam
+          maiores neque non, temporibus cupiditate tenetur dolorem earum.
+        </p>
+      </div>
+      <div class="contenedor-baner-portafolio-imagen" oncontextmenu="return false;">
+        <video controls muted autoplay loop @mouseover="autoPlay" @focus="bar">
+          <source src="../assets/media/videos/videoBaner.mp4" type="video/MP4" />
+        </video>
+      </div>
+    </div>
     <div class="contenedor-habilidades">
       <h1>Habilidades</h1>
       <font-awesome-icon icon="fa-brands fa-vuejs" />
@@ -34,13 +50,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ComponetCardSeccionesVue from '@/components/ComponetCardSecciones.vue';
+import { faVideo } from '@fortawesome/free-solid-svg-icons';
 
-export default defineComponent({
+export default {
   name: 'HomeView',
   components: {
     ComponetCardSeccionesVue,
   },
-});
+  methods: {},
+};
 </script>
 
 <style lang="scss" scoped>
@@ -84,12 +102,45 @@ h1 {
   @include tamanoIconos;
 }
 
+.contenedor-baner-portafolio {
+  display: flex;
+  margin: auto;
+  border: solid 1px white;
+  width: 80%;
+  height: 500px;
+  @include centerFlex;
+}
+
+.contenedor-baner-portafolio-texto {
+  width: 40%;
+  margin: $margin-10;
+}
+
+.contenedor-baner-portafolio-imagen {
+  width: 60%;
+  margin-right: $margin-20;
+  video {
+    width: 100%;
+    margin: auto;
+  }
+}
+
 @media (max-width: 414px) {
   .baner-mensaje {
     width: 60%;
     border-top: 3px solid white;
     margin-top: $margin-10;
     padding: $padding-10 * 2;
+  }
+
+  .contenedor-baner-portafolio {
+    display: flex;
+    flex-direction: column;
+  }
+  .contenedor-baner-portafolio-imagen,
+  .contenedor-baner-portafolio-texto {
+    width: 90%;
+    margin: auto;
   }
 }
 </style>
